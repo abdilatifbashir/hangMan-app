391ded35f79245ac6df4e69ecabb0826b4210553
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-05 19:47:51 +0300

    add getters to my hangman class property accessible by other classes i.e main class

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -8,6 +8,21 @@ public class Hangman{
   Random randomNum=new Random();
   private static String[] guesses={"computer","program","moringa"}
   private static String userWord=guesses[randomNum.nextInt(guesses.length)];
-  private String
+  private String asterisk=new String(new char[userWord.length()]).replace("\0","*");
+  private int counter=0;
+
+  public String[] getGuesses(){
+    return guesses;
+  }
+
+  public String geUserWord(){
+    return userWord;
+  }
+  public String getAsterisk(){
+    return asterisk;
+  }
+  public int GetCounter(){
+    return counter;
+  }
 
 }
