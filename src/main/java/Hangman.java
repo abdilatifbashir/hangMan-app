ab340e46e7f7a26403b92394a4d962155f5173d3
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-05 20:39:24 +0300

    add if statements that set values to asterisk

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -11,6 +11,7 @@ public class Hangman{
   private static String userWord=guesses[randomNum.nextInt(guesses.length)];
   private String asterisk=new String(new char[userWord.length()]).replace("\0","*");
   private int counter=0;
+  private int falseGuesses;
 
   public String[] getGuesses(){
     return guesses;
@@ -26,6 +27,24 @@ public class Hangman{
     return counter;
   }
 public void gamer(String trial){
+  if(userWord.indexOf(trial.charAt(0)==-1)){
+    falseGuesses+=1;
+  }
+  else{
+    alphabet.add(trial);
+  }
+  Sring rightGuesses="";
+  for(i=0;i<userWord.length(); i++){
+    if(userWord.charAt(i)==trial.charAt(0)){
+      rightGuesses+=trial.charAt(0);
+    }
+    elseif(asterisk.charAt(i)!='*'){
+      rightGuesses+=charAt(i);
+    }
+    else{
+      rightGuesses+="*"
+    }
+  }
 
 }
 }
