6d3fdaf91e928eb6cf5b894faa138a231466485f
abdilatifbashir <abdiladifbashir0000@gmail.com>
2017-12-05 19:29:44 +0300

    add random generator that loop or specifies some words in my guesses

--- src/main/java/Hangman.java
+++ src/main/java/Hangman.java
@@ -1,8 +1,13 @@
-import java.util.Scanner;
+
+import java.io.Console;
+import java.util.List;
+import java.util.ArrayList;
 import java.util.Random;
+
 public class Hangman{
-  Scanner scanner=new Scanner(System.in);
-  Random random=new Random();
-  String[] guesses={"computer","program","moringa","school","kenya","table","door","name"};
+  Random randomNum=new Random();
+  private static String[] guesses={"computer","program","moringa"}
+  private static String userWord=guesses[randomNum.nextInt(guesses.length)];
+  private String
 
 }
--- src/test/java/HangmanTest.java
+++ src/test/java/HangmanTest.java
@@ -3,11 +3,12 @@ import static org.junit.Assert.*;
 
 public class HangmanTest {
   @Test
-    public void isHangMan_returning_Arrays() {
+    public void HangMan_should_Arrays()
+    
 
 
 
     }
 
-  
+
 }
